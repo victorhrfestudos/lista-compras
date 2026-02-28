23025cd3926f93cd79dcb1f6456de1c6d0bae3c3
victorhrfestudos <vhugorf.vf@gmail.com>
2026-02-27 21:34:35 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,28 @@
-// Service Worker mínimo
+const CACHE_NAME = 'lista-compras-v1'
+
+const arquivos = [
+    '/',
+    '/index.html',
+    '/historico.html',
+    '/script.js',
+    '/historico.js',
+    '/style.css',
+    '/manifest.json',
+    '/icon.png'
+]
+
 self.addEventListener('install', function(e) {
-    console.log('Service Worker instalado')
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(arquivos)
+        })
+    )
 })
 
 self.addEventListener('fetch', function(e) {
-    // Por enquanto só deixa passar
+    e.respondWith(
+        caches.match(e.request).then(function(resposta) {
+            return resposta || fetch(e.request)
+        })
+    )
 })
